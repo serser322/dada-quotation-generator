abdbdb3bd9113c3e36a348927480c15350d5fe93
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-05-09 01:29:48 +0800

    feat:新增datepicker 匯入datepicker第三方元件 datepicker調整與排版

--- src/views/QuoteInput.vue
+++ src/views/QuoteInput.vue
@@ -1,6 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 import BaseCard from '../components/BaseCard.vue'
 import BaseButton from '../components/BaseButton.vue'
+
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref()
+
+const format = (date) => {
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+
+  return `${year}/${month}/${day}`
+}
 
 </script>
 
@@ -15,9 +29,32 @@ import BaseButton from '../components/BaseButton.vue'
       </div>
       <div class="quote_date">
         <h2>
-          請選擇該名言誕生的日期：
+          請選擇此名言金句誕生日期：
         </h2>
-        <input type="date">
+        <div class="date_input">
+          <VueDatePicker
+            v-model="date"
+            :enable-time-picker="false"
+            auto-apply
+            locale="zh-tw"
+            placeholder="請選擇日期"
+            :format="format"
+            :day-names="['一', '二', '三', '四', '五', '六', '日']"
+            dark
+          >
+            <template #dp-input="{ value }">
+              <input
+                class="date"
+                type="text"
+                :value="value"
+                placeholder="請選擇日期"
+              >
+              <i class="material-symbols-outlined">
+                calendar_month
+              </i>
+            </template>
+          </VueDatePicker>
+        </div>
       </div>
     </BaseCard>
     <BaseButton>
@@ -26,9 +63,8 @@ import BaseButton from '../components/BaseButton.vue'
   </main>
 </template>
 
-<style lang="scss">
-input[type=text],
-input[type=date] {
+<style lang="scss" scoped>
+input[type=text] {
   width: 100%;
   font-size: 1.5rem;
   background-color: transparent;
@@ -38,7 +74,24 @@ input[type=date] {
   &:focus {
     outline: 0;
     border-bottom: 3px solid white;
+  }
+}
 
+.quote_date {
+  margin-top: 4rem;
+
+  .date_input {
+    width: 30%;
+
+    .date {
+      padding-left: 2.5rem;
+    }
+
+    .material-symbols-outlined {
+      position: absolute;
+      top: 1px;
+      left: 0;
+    }
   }
 }
 </style>
