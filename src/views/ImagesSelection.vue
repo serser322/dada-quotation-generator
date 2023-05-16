b739e9c18b8b1a7f245a25fe6acc26ef49466460
Wei Chiao Chen <joess.cts02@g2.nctu.edu.tw>
2023-05-16 09:04:29 +0800

    feat:選圖頁切版 新增card與圖片

--- src/views/ImagesSelection.vue
+++ src/views/ImagesSelection.vue
@@ -1,13 +1,43 @@
 <script setup>
+// import { ref } from 'vue'
+import BaseCard from '../components/BaseCard.vue'
 
+// const imagesData = ref([
+
+// ])
 </script>
 
 <template>
   <main class="images-selection">
-    <p>Images selection</p>
+    <BaseCard>
+      <h2 for="">
+        請選擇此名言圖上的立繪：
+      </h2>
+      <div class="images">
+        <div>
+          <img
+            src="../assets/dada.png"
+            alt=""
+          >
+        </div>
+      </div>
+    </BaseCard>
   </main>
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+.images {
+  div {
+    width: 12rem;
+    border: 3px solid white;
+    border-radius: 1rem;
+    background-color: rgb(122, 122, 122);
+
+    img {
+      width: 100%;
+    }
+  }
+}
 
 </style>
